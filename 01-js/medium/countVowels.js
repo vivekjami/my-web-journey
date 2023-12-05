d67aa4600082adc7e33c722132624b25ae049871
JAMI VIVEK <109971110+vivekjami@users.noreply.github.com>
2023-12-05 20:46:01 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,15 @@
 
 function countVowels(str) {
     // Your code here
+    let string1 = str;
+    let string2 = string1.toLowerCase();
+    let count = 0;
+    for(let i=0;i<string2.length;i++){
+        if(string2[i]==='a'||string2[i]==='e'||string2[i]==='i'||string2[i]==='o'||string2[i]==='u'){
+            count++;
+        }
+    }
+    return count;
 }
 
 module.exports = countVowels;
